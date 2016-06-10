aa187fd3dd0f443d8660720872df904db21a15fd
Michael Zhang <me@michaelz.xyz>
2016-06-09 21:35:21 -0500

    Fixed something?

--- cal.py
+++ cal.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-runner = Runner(app)
 
 @app.route("/")
 def index():
