80f0243180550f016e79945588e3d46f2a2a5a8f
David Hou <david.hou314@gmail.com>
2016-06-09 23:05:46 -0700

    Vagrant setup

--- Vagrantfile
+++ Vagrantfile
@@ -69,4 +69,5 @@ Vagrant.configure(2) do |config|
   #   sudo apt-get update
   #   sudo apt-get install -y apache2
   # SHELL
+  config.vm.provision "shell", path: "setup.sh"
 end
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MYSQL_ROOT_PASSWORD=`cat /vagrant/MYSQL_ROOT_PASSWORD`
+
+echo "Updating system..."
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+echo "Preparing for MySQL installation..."
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+echo "Installing dependencies..."
+sudo apt-get install -y libffi=3.1~rc1+r3.0.13-12ubuntu0.1
+sudo apt-get install -y mysql-server
+
+echo "Installing pip dependencies..."
+sudo pip3 install -r /vagrant/requirements.txt
+
+echo "PATH=$PATH:/vagrant" >> /etc/profile
+source /etc/profile
+
+sudo sed -i s/127.0.0.1/0.0.0.0/ /etc/mysql/my.cnf
+sudo service mysql restart
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+CREATE DATABASE easyctf-cal;
+EOF
