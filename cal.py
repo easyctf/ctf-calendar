e8bfd1275b1430472b286d89d8b36fcc244467fd
David Hou <david.hou314@gmail.com>
2016-06-09 19:30:41 -0700

    Add db and manage.py

--- cal.py
+++ cal.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_runner import Runner
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 runner = Runner(app)
+
 
 @app.route("/")
 def index():
 	return "Hello World!"
+
 
 if __name__ == "__main__":
 	runner.run()
