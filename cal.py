23430fcda2296dff7eea1d6f8e302c818a7d6968
David Hou <david.hou314@gmail.com>
2016-06-09 19:40:05 -0700

    Add db config and mysql setup things

--- cal.py
+++ cal.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Flask
 from flask_runner import Runner
 from flask_sqlalchemy import SQLAlchemy
 
+import config
+
 app = Flask(__name__)
+self_path = os.path.dirname(os.path.abspath(__file__))
+app.config.from_object(config.CalendarConfig(app_root=self_path))
 db = SQLAlchemy(app)
 runner = Runner(app)
 
