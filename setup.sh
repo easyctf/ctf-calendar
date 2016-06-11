f1828f2f3ffd77ef1d6f55624824ff231a26340b
Michael Zhang <me@michaelz.xyz>
2016-06-11 13:40:02 -0500

    (wip) login page

--- setup.sh
+++ setup.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-MYSQL_ROOT_PASSWORD=`cat /vagrant/MYSQL_ROOT_PASSWORD`
-
 echo "Updating system..."
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get -y update
+apt-get -y upgrade
 
-echo "Preparing for MySQL installation..."
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+apt-get -y install python-dev python-pip libffi-dev
+apt-get -y install postgresql postgresql-contrib libpq-dev
 
-echo "Installing dependencies..."
-sudo apt-get install -y libffi=3.1~rc1+r3.0.13-12ubuntu0.1
-sudo apt-get install -y mysql-server
-
-echo "Installing pip dependencies..."
-sudo pip2 install -r /vagrant/requirements.txt
-
-echo "PATH=$PATH:/vagrant" >> /etc/profile
-source /etc/profile
-
-sudo sed -i s/127.0.0.1/0.0.0.0/ /etc/mysql/my.cnf
-sudo service mysql restart
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
-CREATE DATABASE easyctf_cal;
-EOF
+pip install -r requirements.txt
