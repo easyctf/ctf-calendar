b943d698fb52f5a2a1384f74be947a147da22a86
David Hou <david.hou314@gmail.com>
2016-06-10 17:12:43 -0700

    Fix mergefail

--- cal.py
+++ cal.py
@@ -11,17 +11,8 @@ app = Flask(__name__)
 self_path = os.path.dirname(os.path.abspath(__file__))
 app.config.from_object(config.CalendarConfig(app_root=self_path))
 
-db = SQLAlchemy(app)
-
 db.init_app(app)
 login_manager.init_app(app)
 app.register_blueprint(blueprint_base)
 app.register_blueprint(blueprint_users)
 app.register_blueprint(blueprint_events, url_prefix='/events')
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
